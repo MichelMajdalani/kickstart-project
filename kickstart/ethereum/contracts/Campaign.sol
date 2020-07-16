2a566d9492035f42ec5b807102a5d3cce4fd2e79
MichelMajdalani <michel.majdalani@mail.mcgill.ca>
2020-07-16 19:24:57 -0400

    Add web3 and continued tests

--- kickstart/ethereum/contracts/Campaign.sol
+++ kickstart/ethereum/contracts/Campaign.sol
@@ -61,7 +61,9 @@ contract Campaign {
     /// @dev Design pattern used with mapping to account for the mapping's length without iteration
     uint256 public approversCount;
     /// @dev Withdrawal design pattern
-    mapping(address => uint256) pendingWithdrawals;
+    mapping(address => uint256) public pendingWithdrawals;
+    /// @dev Counter that follows life of contract balance
+    uint256 public balance;
 
     event NewContribution(uint256 amount);
     event NewContributor(address contributor);
@@ -94,6 +96,7 @@ contract Campaign {
         }
 
         approvers[msg.sender] = true;
+        balance += msg.value;
         emit NewContribution(msg.value);
     }
 
@@ -131,13 +134,29 @@ contract Campaign {
     function finalizeRequest(uint256 index) public restricted {
         Request storage request = requests[index];
 
-        require(request.approvalCount > (approversCount/2), 'At least 50% of the approvers must accept the request for it to pass.');
+        require(request.value < balance, "Attempt to finalize an infeasible request");
+        require(request.approvalCount > (approversCount/2), 'More than 50% of the approvers must accept the request for it to pass.');
         require(!request.complete, 'Request has already been completed.');
 
-        // SafeMath is not needed since we only add the amount once.
+        balance -= request.value;
+        // TODO Add Safe Math
         pendingWithdrawals[request.recipient] += request.value;
         request.complete = true;
         emit SubmittedRequestToRecipient(request.recipient, request.value);
+    }
+
+    // TODO This feature might be deleted
+    /// @notice Cancel finalization of request
+    /// @param index Request's id
+    function cancelRequest(uint256 index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.complete, 'Request is not complete.');
+
+        request.complete = false;
+        balance += request.value;
+        pendingWithdrawals[request.recipient] -= request.value;
+        // TODO reset mapping to default configuration (to be able to revote)
     }
 
     /// @notice Allow a request's recipient to withdraw value
--- kickstart/ethereum/test/Campaign.test.js
+++ kickstart/ethereum/test/Campaign.test.js
@@ -1,14 +1,44 @@
 const Campaign = artifacts.require("Campaign");
 
+let BN = web3.utils.BN;
+
 
 contract("Campaign", accounts => {
     // Start from a fresh state
     let campaign;
+    let index;
+
     let manager = accounts[0];
     let minimumContribution = web3.utils.toWei('1', 'nano');
+    let recipient = accounts[2];
+    // TODO change to a parameter or find a better way of doing this
+    let requestValue = minimumContribution;
+
     beforeEach(async () => {
         campaign = await Campaign.new(minimumContribution, manager);
+        index=0;
     })
+
+    // Utility functions
+    function createPaymentRequest() {
+        let previousIndex = index;
+        campaign.createRequest('Buy batteries', requestValue, recipient, { from: manager }).then(() => index++);
+        return previousIndex;
+    }
+
+    function approvePaymentRequest() {
+        console.log('Here!');
+        const index = createPaymentRequest();
+        contributorList = [accounts[1], accounts[2], accounts[3]];
+
+        for(contributor of contributorList) {
+            campaign.contribute({ value: minimumContribution, from: contributor})
+                .then(() => {});
+            campaign.approveRequest(index, {from: contributor}).then(() => {});
+
+        }
+        return index;
+    }
 
     it('marks both correct manager and contribution', async () => {
         let owner = await campaign.manager();
@@ -57,60 +87,183 @@ contract("Campaign", accounts => {
         assert.strictEqual(approversCount.toNumber(), 1, 'Did not count duplicate contributor');
     });
 
-    it('requires minimum constribution', async () => {
+    it('requires minimum contribution', async () => {
         try {
-            let BN = web3.utils.BN;
-            let smallerAmount = minimumContribution.sub(new BN('1'));
+            let smallerAmount = new BN(minimumContribution).sub(new BN('1'));
             await campaign.contribute({
                 value: smallerAmount,
                 from: accounts[1]
             });
             assert.fail("Possible to contribute with less than minimum contribution");
         } catch (err) {
-            // TODO Catch and compare the error message to 'The contribution amount is lower than the minimum accepted.' 
-            assert(err);
+            assert.include(err.message, 'The contribution amount is lower than the minimum accepted.', 'Cannot contribute with less than minimum contribution');
         }
     });
 
-    it('allows a manager to make a payment request', async () => {
-        await campaign.createRequest('Buy batteries', '1000', accounts[2], { from: accounts[0] });
-        const request = await campaign.requests(0); // or requests()[0]
+    it('allows a manager to make a payment request success', async () => {
+        const index = createPaymentRequest();
+        const request = await campaign.requests(index); // or requests()[0]
         assert.strictEqual(request.description, 'Buy batteries', 'Correct description');
-        assert.strictEqual(request.value.toString(), '1000', 'Correct value');
+        assert.strictEqual(request.value.toString(), requestValue.toString(), 'Correct value');
         assert.strictEqual(request.recipient, accounts[2], 'Correct recipient');
 
     });
 
-    // TODO Add try creating a request when not the manager
-    // TODO Continue writing tests
-    // it('processes requests', async () => {
-    //     await campaign.methods.contribute().send({
-    //     from: accounts[0],
-    //     value: web3.utils.toWei('10', 'ether')
-    //     });
+    it('restrict a non-manager to make a payment request', async () => {
+        try {
+            await campaign.createRequest('Buy batteries', '1000', accounts[2], { from: accounts[1] });
+            assert.fail("Possible to create a request when not a manager");
+        } catch (err) {
+            assert.include(err.message, "You do not have manager privileges.", 'Permission restricted');            
+        }
+    });
+    
+    it('approve request as contributor', async () => {
+        const index = createPaymentRequest();
+        let contributor = accounts[1];
 
-    //     await campaign.methods
-    //     .createRequest("A", web3.utils.toWei('5', 'ether'), accounts[1])
-    //     .send({
-    //         from: accounts[0],
-    //         gas: '1000000'
-    //     });
+        // Assert 0 initially
+        let request = await campaign.requests(index);
+        assert.strictEqual(request.approvalCount.toNumber(), 0, "Approval count initialized to 0");
+        
+        // assert.isFalse(request.approvals(contributor), "No approvers"); // need to write custom get
 
-    //     await campaign.methods.approveRequest(0).send({
-    //     from: accounts[0],
-    //     gas: '1000000'
-    //     });
+        // Contribute and approve
+        await campaign.contribute({ value: minimumContribution, from: contributor});
+        await campaign.approveRequest(index, {from: contributor});
+        
+        // Assert 1 after approval
+        request = await campaign.requests(index);
+        assert.strictEqual(request.approvalCount.toNumber(), 1, "Incremented approval count");
+        // assert.isTrue(request.approvals(contributor), "Set contributor as approver"); // need to write custom get
 
-    //     await campaign.methods.finalizeRequest(0).send({
-    //     from: accounts[0],
-    //     gas: '1000000'
-    //     });
+    });
 
-    //     let balance = await web3.eth.getBalance(accounts[1]); // in Wei
-    //     balance = web3.utils.fromWei(balance, 'ether');
-    //     balance = parseFloat(balance);
-    //     console.log(balance);
-    //     assert(balance > 104);
+    it('restrict approval when not a contributor', async () => {
+        const index = createPaymentRequest();
+
+        // Contribute and approve
+        let fakecontributor = accounts[1];
+        try {
+            await campaign.approveRequest(index, {from: fakecontributor});
+            assert.fail("Possible to approve a request when you are not a contributor");
+        } catch(err) {
+            assert.include(err.message, 'You are not a contributor to this campaign.');
+        }
+    });
+
+    it('restrict approval when duplicate approvals contributor', async () => {
+        const index = createPaymentRequest();
+
+        // Contribute and approve
+        let contributor = accounts[1];
+        await campaign.contribute({ value: minimumContribution, from: contributor});
+        await campaign.approveRequest(index, {from: contributor});
+
+        try {
+            await campaign.approveRequest(index, {from: contributor});
+            assert.fail("Possible to approve a request twice");
+        } catch(err) {
+            assert.include(err.message, 'You have already approved this request.');
+        }
+    });
+
+    // it('test approve request', async () => {
+    //     approvePaymentRequest();
+    // });
+
+    // it('finalize request', async () => {
+    //     const index = approvePaymentRequest();
+    //     let balance = await campaign.balance();
+    //     // TODO Not hardcode to 3
+    //     let totalContributions = new BN(minimumContribution).mul(new BN('3'));
+    //     assert.strictEqual(balance.toNumber(), totalContributions.toNumber(), "balance accounted properly");
+
+    //     const tx = await campaign.finalizeRequest(index, {from: manager});
+        
+    //     // Event triggered
+    //     assert.strictEqual(tx.logs[0].event, "SubmittedRequestToRecipient");
+
+    //     // Request status
+    //     const request = await campaign.requests(index);
+    //     assert.isTrue(request.complete, "Request completed");
+
+    //     // Balance
+    //     totalContributions = new BN(totalContributions).sub(new BN(request.value));
+    //     balance = await campaign.balance();
+    //     assert.strictEqual(balance.toNumber(), totalContributions.toNumber(), "balance accounted properly after finalization");
+        
+    //     // Pending withdrawals
+    //     const withdrawalAmount = await campaign.pendingWithdrawals(recipient);
+    //     assert.strictEqual(withdrawalAmount.toString(), request.value.toString(), "Set withdrawal ammount correctly");
 
     // });
+
+    // it('restrict finalize request when not a manager', async () => {
+    //     const index = approvePaymentRequest();
+    //     try {
+    //         await campaign.finalizeRequest(index, {from: recipient});
+    //         assert.fail("Can finalize a request when you are not the manager");
+    //     } catch (err) {
+    //         assert.include(err.message, "You do not have manager privileges.", "Modifier set in finalize request");
+    //     }
+    // });
+
+    // it('finalize request when funds are not enough', async () => {
+    //     // Modify request value to exceed contributions
+    //     requestValue = new BN(minimumContribution).mul(new BN('10'));
+    //     let oldWithdrawalAmount = await campaign.pendingWithdrawals(recipient);
+    //     approvePaymentRequest();
+    //     try {
+    //         await campaign.finalizeRequest(index, {from: manager});
+    //         assert.fail("Can finalize a request when you have no money for it");
+    //     } catch (err) {
+    //         let newWithdrawalAmount = await campaign.pendingWithdrawals(recipient);
+    //         assert.strictEqual(oldWithdrawalAmount, newWithdrawalAmount, "No withdrawal added");
+    //         assert.include(err.message, "Attempt to finalize an infeasible request", "No money creation");
+    //     }
+    // });
+
+    // it('duplicate finalize request', async () => {
+    //     const index = approvePaymentRequest();
+    //     await campaign.finalizeRequest(index, {from: manager});
+    //     try {
+    //         await campaign.finalizeRequest(index, {from: manager});
+    //         assert.fail("Can finalize a request twice");
+    //     } catch (err) {
+    //         assert.include(err.message, 'Request has already been completed.', "No duplicate request");
+    //     }
+    // });
+
+    // it('finalize request when the majority did not accept yet', async () => {
+    //     const index = approvePaymentRequest();
+    //     // Add a contributor that doesn't approve
+    //     await campaign.contribute({ value: minimumContribution, from: accounts[4]});
+    //     try {
+    //         await campaign.finalizeRequest(index, {from: manager});
+    //         assert.fail("Can finalize a request twice");
+    //     } catch (err) {
+    //         assert.include(err.message, 'More than 50% of the approvers must accept the request for it to pass.', "Preserve democracy");
+    //     }
+    // });
+
+    // it('withdraw', async () => {
+    //     // TODO
+    // });
+
+    // it('malintentioned withdraw', async () => {
+    //     // TODO
+    // });
+
+    // it('get request count', async () => {
+    //     // TODO
+    // });
+
+    // it('get summary', async () => {
+    //     // TODO
+    // });
+
+    // TODO Test cancelRequest function tests
+    // TODO Check that if you call createrequest twice it updates properly the index
+    // TODO 
 });
