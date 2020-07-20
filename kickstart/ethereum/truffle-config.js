d5c8ce6eda4b36c97fad24251b894c6f155e72eb
MichelMajdalani <michel.majdalani@mail.mcgill.ca>
2020-07-19 21:11:38 -0400

    Set up and migrated to rinkeby

--- kickstart/ethereum/truffle-config.js
+++ kickstart/ethereum/truffle-config.js
@@ -18,11 +18,9 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config()
+const mnemonic = process.env.MNEMONIC.trim();
 
 module.exports = {
   /**
@@ -60,14 +58,14 @@ module.exports = {
 
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-      // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-      // network_id: 3,       // Ropsten's id
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/e8f9af3bc18943e984ef0e41daf0dd1c`),
+      network_id: 4,       // Rinkeby's id
+      gas: 5500000,        // Rinkeby has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+    },
 
     // Useful for private networks
     // private: {
---
+++ kickstart/ethereum/web3.js
@@ -0,0 +1,18 @@
+import detectEthereumProvider from '@metamask/detect-provider'
+import Web3 from 'web3'
+
+/**
+ * Supports only Metamask for now
+ */
+export default async function () {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+        console.log('Ethereum successfully detected!');
+        await provider.enable();
+        return new Web3(provider);
+    } else {
+        // TODO Handle more gracefully
+        // if the provider is not detected, detectEthereumProvider resolves to null
+        console.error('Please install MetaMask!', error);
+    }
+}
