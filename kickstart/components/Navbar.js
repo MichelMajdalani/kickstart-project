22599a0deb6c303f9c2e9cb43dc93f473b86008a
MichelMajdalani <michel.majdalani@mail.mcgill.ca>
2020-08-20 17:23:35 -0400

    Refactored components

--- kickstart/components/Navbar.js
+++ kickstart/components/Navbar.js
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default class NavigationBar extends React.Component {
     constructor(props) {
@@ -7,35 +8,26 @@ export default class NavigationBar extends React.Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-md navbar-light bg-light">
+            <Navbar bg="light" expand="md">
                 <Link href="/">
-                    <a className="navbar-brand">
+                    <Navbar.Brand className="navbar-brand">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-wallet-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1.5 2A1.5 1.5 0 0 0 0 3.5v2h6a.5.5 0 0 1 .5.5c0 .253.08.644.306.958.207.288.557.542 1.194.542.637 0 .987-.254 1.194-.542.226-.314.306-.705.306-.958a.5.5 0 0 1 .5-.5h6v-2A1.5 1.5 0 0 0 14.5 2h-13z"/>
                             <path d="M16 6.5h-5.551a2.678 2.678 0 0 1-.443 1.042C9.613 8.088 8.963 8.5 8 8.5c-.963 0-1.613-.412-2.006-.958A2.679 2.679 0 0 1 5.551 6.5H0v6A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6z"/>
                         </svg>
                         &nbsp; Kick Kick Starter
-                    </a>
+                    </Navbar.Brand>
                 </Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link href="/">
-                            <a className="nav-link">Home<span className="sr-only">(current)</span></a>
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contact Us</a>
-                    </li>
-                    </ul>
-                </div>
-            </nav>
+                <Navbar.Toggle aria-controls="navbar-collapse" />
+                <Navbar.Collapse id="navbar-collapse">
+                    <Nav className="mr-auto">
+                    {/* Add link and bold (current) */}
+                        <Nav.Link>Home</Nav.Link>
+                        <Nav.Link>About</Nav.Link>
+                        <Nav.Link>Contact Us</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
